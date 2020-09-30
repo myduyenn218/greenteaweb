66cf2e032e4bda7127745b954d3eeb5331239252
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2020-09-30 14:53:49 +0700

    add midelware

---
+++ app/Http/Middleware/Admin/CheckLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware\Admin;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user =\session('auth');
+        if($user!=null){
+            return $next($request);
+        }else{
+            return redirect('/login');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth;
 use App\Http\Controllers\User;
+use App\Http\Controllers\Admin;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,10 +36,12 @@ Route::get('/activity', "App\\Http\\Controllers\\ActivityController@activity");
 Route::get('/page-create', function () {
     return view('page-creator');
 });
+Route::get('/logout',[Auth\LoginControler::class,'logout']);
 Route::get('/my-project', function () {
     return view('my-project');
 });
-Route::get('/profile', [User\Profile::class,'showProfile']);
+Route::get('/profile', [User\Profile::class,'showProfile'])->middleware(\App\Http\Middleware\Admin\CheckLogin::class);
 
-Route::post('/profile', [User\Profile::class,'changeProfile'])->name('changprofile');
+Route::post('/profile', [User\Profile::class,'changeProfile'])->name('changprofile')->middleware(\App\Http\Middleware\Admin\CheckLogin::class);
 
+Route::get('/createpage',[Admin\ManagePage::class,'showCreatePage'])->middleware(\App\Http\Middleware\Admin\CheckLogin::class);
